7af9f1d5abcb1a495bef6042ada5724fb0a96742
hle <huy.le@sony.com>
2017-02-11 14:49:01 -0800

    added bst more method

--- Data-Structure-And-Algorithms/bst/bst.go
+++ Data-Structure-And-Algorithms/bst/bst.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-// "fmt"
+	"fmt"
 )
 
 // TreeNode is a representation of a node in a tree
@@ -9,19 +9,64 @@ import (
 type TreeNode struct {
 	left  *TreeNode
 	right *TreeNode
+	key   int
 	value int
 }
 
 // Bst Binary  Search Tree Implementation
+// Please write implementation
+// At the end please put time complexity for each
+// operations
 type Bst struct {
 	head *TreeNode
 }
 
 // Insert into the BST
-func (t *Bst) Insert(value int) {
+func (t *Bst) Insert(key, value int) {
 }
 
-// TraverseAndPrintPretty traverse in-order and print the tree out
-// pretty
-func (t *Bst) TraverseAndPrintPretty() {
+// Search will seach for an item with key
+// and return the value
+func (t *Bst) Search(key int) int {
+
+	return 0
+}
+
+// Delete will seach for an item with key
+// delete the item, and then return the value
+func (t *Bst) Delete(key int) int {
+	return 0
+}
+
+// FindMax returns the value of the max key
+func (t *Bst) FindMax() int {
+
+}
+
+// InOrderRec in-order recursively traversal
+func (t *Bst) InOrderRec() {
+}
+
+// PreOrderRec pre-order recursively traversal
+func (t *Bst) PreOrderRec() {
+}
+
+// PostOrderRec post-order recursively traversal
+func (t *Bst) PostOrderRec() {
+}
+
+// InOrderIter in-order iteratively == DFS1
+func (t *Bst) InOrderIter() {
+}
+
+// PreOrderIter pre-order iteratively == DFS2
+func (t *Bst) PreOrderIter() {
+}
+
+// PostOrderIter pre-order iteratively == DFS3
+func (t *Bst) PostOrderIter() {
+}
+
+// BFS Breath First Search
+func (t *Bst) BFS() {
 }
