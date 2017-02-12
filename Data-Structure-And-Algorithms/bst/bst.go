bf476e521ef7a2f2bd467f1a64c00ffb2d038e42
hle <huy.le@sony.com>
2017-02-12 14:07:34 -0800

    long journey of bst you know

--- Data-Structure-And-Algorithms/bst/bst.go
+++ Data-Structure-And-Algorithms/bst/bst.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"reflect"
 )
 
 // TreeNode is a representation of a node in a tree
@@ -43,19 +44,134 @@ func (t *Bst) Insert(key, value int) {
 			node = node.left
 		}
 	}
+	// O(logn)
 }
 
 // Search will seach for an item with key
 // and return the value
-func (t *Bst) Search(key int) int {
+func (t *Bst) Search(key int) (bool, int) {
+	if t.head == nil {
+		return false, 0
+	}
 
-	return 0
+	node := t.search(key)
+
+	if node == nil {
+		return false, 0
+	}
+	return true, node.value
+	// O(logn)
+}
+
+func (t *Bst) search(key int) *TreeNode {
+	node := t.head
+	for node != nil && node.key != key {
+		if key > node.key {
+			node = node.right
+		} else {
+			node = node.left
+		}
+	}
+	return node
+}
+
+func (t *Bst) searchPrev(key int) *TreeNode {
+	node := t.head
+	var prev *TreeNode
+	prev = nil
+	for node != nil && node.key != key {
+		if key > node.key {
+			prev = node
+			node = node.right
+		} else {
+			prev = node
+			node = node.left
+		}
+	}
+	return prev
 }
 
 // Delete will seach for an item with key
 // delete the item, and then return the value
-func (t *Bst) Delete(key int) int {
-	return 0
+func (t *Bst) Delete(key int) (bool, int) {
+	prev := t.searchPrev(key)
+	found := t.search(key)
+	if found == nil {
+		return false, 0
+	}
+
+	///////////////////////
+	// no child on found //
+	///////////////////////
+
+	if found.left == nil && found.right == nil {
+		if prev == nil { // prev == nil means we are trying to delete the head node
+			t.head == nil
+			return true, found.value
+		}
+		// found is not head
+		if prev.left.key == found.key { // found on the left of previous
+			prev.left = nil
+		} else { // found on the right of previous
+			prev.right = nil
+		}
+		return true, found.value
+	}
+
+	///////////////////////////////
+	// exactly one child on found//
+	///////////////////////////////
+
+	if found.left == nil || found.right == nil {
+		// found is the t.head
+		if prev == nil { // meaning this is the head node
+			if found.left == nil {
+				t.head = found.right
+			} else {
+				t.head = foudn.left
+			}
+			return true, found.value
+		}
+		// found is not head
+		if prev.left.key == found.key { // found on the left of previous
+			if found.left == nil { // left of found is nil,
+				prev.left = found.right
+			} else {
+				prev.left = found.left
+			}
+		} else { // found on the right of previous
+			if found.left == nil { // left of found is nil,
+				prev.right = found.right
+			} else {
+				prev.right = found.left
+			}
+		}
+		return true, found.value
+	}
+
+	///////////////////////////////////
+	// two child on found /////////////
+	///////////////////////////////////
+
+	max, previous := findMaxUnderNode(found.left)
+	found.value = max.value
+	found.key = max.key
+	if previous != nil {
+		previous.right = max.left
+	} else {
+		found.left = nil
+	}
+	return true, found.value
+}
+
+func (t *Bst) findMaxUnderNode(node *TreeNode) (*TreeNode, *TreeNode) {
+	var prev *TreeNode
+	prev = nil
+	for node.right != nil {
+		previous = node
+		node = node.right
+	}
+	return node, prev
 }
 
 // FindMax returns the value of the max key
@@ -64,39 +180,62 @@ func (t *Bst) FindMax() int {
 }
 
 // InOrderRec in-order recursively traversal
-func (t *Bst) InOrderRec() {
-	inorderRecHelper(t.head)
+func (t *Bst) InOrderRec(f func(key int)) {
+	inorderRecHelper(t.head, f)
+	// O(n)
 }
 
-func inorderRecHelper(node *TreeNode) {
+func inorderRecHelper(node *TreeNode, f func(key int)) {
 	if node == nil {
 		return
 	}
-	inorderRecHelper(node.left)
-	fmt.Print(node.value, " ")
-	inorderRecHelper(node.right)
+	inorderRecHelper(node.left, f)
+	f(node.key)
+	inorderRecHelper(node.right, f)
 }
 
 // PreOrderRec pre-order recursively traversal
-func (t *Bst) PreOrderRec() {
+func (t *Bst) PreOrderRec(f func(key int)) {
+	preorderRecHelper(t.head, f)
+	// This method will take the O(n)
+}
+
+func preorderRecHelper(node *TreeNode, f func(key int)) {
+	if node == nil {
+		return
+	}
+	f(node.key)
+	preorderRecHelper(node.left, f)
+	preorderRecHelper(node.right, f)
 }
 
 // PostOrderRec post-order recursively traversal
-func (t *Bst) PostOrderRec() {
+func (t *Bst) PostOrderRec(f func(key int)) {
+	postorderRecHelper(t.head, f)
+	// This method will take the O(n)
 }
 
-// InOrderIter in-order iteratively == DFS1
+func postorderRecHelper(node *TreeNode, f func(key int)) {
+	if node == nil {
+		return
+	}
+	postorderRecHelper(node.left, f)
+	postorderRecHelper(node.right, f)
+	f(node.key)
+}
+
+// InOrderIter in-order iteratively == DFS1 Stack
 func (t *Bst) InOrderIter() {
 }
 
-// PreOrderIter pre-order iteratively == DFS2
+// PreOrderIter pre-order iteratively == DFS2 Stack
 func (t *Bst) PreOrderIter() {
 }
 
-// PostOrderIter pre-order iteratively == DFS3
+// PostOrderIter pre-order iteratively == DFS3 Stack
 func (t *Bst) PostOrderIter() {
 }
 
-// BFS Breath First Search
+// BFS Breath First Search using a Queue
 func (t *Bst) BFS() {
 }
--- Data-Structure-And-Algorithms/bst/bst_test.go
+++ Data-Structure-And-Algorithms/bst/bst_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -11,6 +13,112 @@ func TestInsert(t *testing.T) {
 	tree.Insert(3, 3)
 	tree.Insert(1, 1)
 	tree.Insert(2, 1)
-	tree.InOrderRec()
-	fmt.Println()
+	slice := []int{}
+	f := func(key int) {
+		slice = append(slice, key)
+	}
+	tree.InOrderRec(f)
+	check := sort.IsSorted(sort.IntSlice(slice))
+	if !check {
+		t.Error("the slice is not sorted, it is ", slice)
+	}
+}
+
+func TestInsertAlotInOrder(t *testing.T) {
+	tree := Bst{}
+	tree.Insert(5, 1)
+	tree.Insert(2, 2)
+	tree.Insert(7, 3)
+	tree.Insert(1, 4)
+	tree.Insert(4, 5)
+	tree.Insert(6, 6)
+	tree.Insert(9, 7)
+	tree.Insert(8, 8)
+	tree.Insert(10, 9)
+	slice := []int{}
+	f := func(key int) {
+		slice = append(slice, key)
+	}
+	tree.InOrderRec(f)
+	check := sort.IsSorted(sort.IntSlice(slice))
+	if !check {
+		t.Error("the slice is not sorted, it is ", slice)
+	}
+}
+
+func TestInsertAlotPreOrder(t *testing.T) {
+	tree := Bst{}
+	tree.Insert(5, 1)
+	tree.Insert(2, 2)
+	tree.Insert(7, 3)
+	tree.Insert(1, 4)
+	tree.Insert(4, 5)
+	tree.Insert(6, 6)
+	tree.Insert(9, 7)
+	tree.Insert(8, 8)
+	tree.Insert(10, 9)
+	slice := []int{}
+	f := func(key int) {
+		slice = append(slice, key)
+	}
+	tree.PreOrderRec(f)
+	reflect.DeepEqual(slice, []int{5, 2, 1, 4, 7, 6, 9, 8, 10})
+}
+
+func TestInsertAlotPostOrder(t *testing.T) {
+	tree := Bst{}
+	tree.Insert(5, 1)
+	tree.Insert(2, 2)
+	tree.Insert(7, 3)
+	tree.Insert(1, 4)
+	tree.Insert(4, 5)
+	tree.Insert(6, 6)
+	tree.Insert(9, 7)
+	tree.Insert(8, 8)
+	tree.Insert(10, 9)
+	slice := []int{}
+	f := func(key int) {
+		slice = append(slice, key)
+	}
+	tree.PostOrderRec(f)
+	reflect.DeepEqual(slice, []int{1, 4, 2, 6, 8, 10, 9, 7, 5})
+}
+
+func TestSearchFound(t *testing.T) {
+	tree := Bst{}
+	tree.Insert(5, 1)
+	tree.Insert(2, 2)
+	tree.Insert(7, 3)
+	tree.Insert(1, 4)
+	tree.Insert(4, 5)
+	tree.Insert(6, 6)
+	tree.Insert(9, 7)
+	tree.Insert(8, 8)
+	tree.Insert(10, 9)
+	found, value := tree.Search(10)
+
+	if !found {
+		t.Error("key not found, it should be found")
+	}
+	if value != 9 {
+		t.Error("value should be 9 but it is ", value)
+	}
+}
+
+func TestSearchNotFound(t *testing.T) {
+	tree := Bst{}
+	tree.Insert(5, 1)
+	tree.Insert(2, 2)
+	tree.Insert(7, 3)
+	tree.Insert(1, 4)
+	tree.Insert(4, 5)
+	tree.Insert(6, 6)
+	tree.Insert(9, 7)
+	tree.Insert(8, 8)
+	tree.Insert(10, 9)
+	found, _ := tree.Search(0)
+
+	if found {
+		t.Error("key should not be found but it is found.")
+	}
 }
